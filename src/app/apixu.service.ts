965db673274a8e1ea1606d7c04d427cd07310f2e
RamkishoreBandla <bandlaramkishore090@gmail.com>
2020-07-04 00:25:09 +0530

    weather 1st commit

---
+++ src/app/apixu.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApixuService {
+ 
+
+  constructor(private http: HttpClient) { }
+
+    getWeather(location)
+    {
+      return this.http.get("https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=23624c5858ee047d959d79dfd9ba3844&query=" + location);
+    }
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ApixuService } from "./apixu.service";
+import { ReactiveFormsModule } from '@angular/forms';
+import { allAppRoutes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApixuService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
